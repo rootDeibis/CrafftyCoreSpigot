1262c93880de1ef59da884c43807bff40b9354ce
Deibis Alejandro <35423510+rootDeibis@users.noreply.github.com>
2023-07-28 09:49:50 -0400

    - Modifications

---
+++ src/main/java/me/rootdeibis/crafftty/commands/core/SubCommands.java
@@ -0,0 +1,12 @@
+package me.rootdeibis.crafftty.commands.core;
+
+
+import me.rootdeibis.crafftty.core.commands.annotations.CoreCommand;
+
+public class SubCommands {
+
+    @CoreCommand(name = "protection", permission = "core.subcmd.protection", aliases = "protect")
+    public void subcommandProtection() {
+
+    }
+}
